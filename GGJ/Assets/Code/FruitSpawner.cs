fd3313a96251c66f926034f950fd9c78ce0f3d73
joiny7 <joiny4u@hotmail.com>
2019-01-25 21:20:02 +0100

    FruitSpawner

---
+++ GGJ/Assets/Code/FruitSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitSpawner : MonoBehaviour {
+
+    public GameObject fruitPrefab;
+    public GameObject parent;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SpawnFruit()
+    {
+        Vector3 spawnPos = parent.transform.position;
+        spawnPos.x += Random.Range(-0.3f, 0.3f);
+        spawnPos.z += Random.Range(-0.3f, 0.3f);
+        var fruit = Instantiate(fruitPrefab, spawnPos, Quaternion.identity);
+    }
+}
