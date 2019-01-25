2f6e8ddd59900feeffd448d1710fbba9b476929c
joiny7 <joiny4u@hotmail.com>
2019-01-25 21:45:55 +0100

    for small trees

--- GGJ/Assets/Code/FruitSpawner.cs
+++ GGJ/Assets/Code/FruitSpawner.cs
@@ -5,22 +5,11 @@ using UnityEngine;
 public class FruitSpawner : MonoBehaviour {
 
     public GameObject fruitPrefab;
-    public GameObject parent;
-
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Transform[] points;
 
     public void SpawnFruit()
     {
-        Vector3 spawnPos = parent.transform.position;
-        spawnPos.x += Random.Range(-0.3f, 0.3f);
-        spawnPos.z += Random.Range(-0.3f, 0.3f);
+        Vector3 spawnPos = points[Random.Range(0, points.Length)].position;
         var fruit = Instantiate(fruitPrefab, spawnPos, Quaternion.identity);
     }
 }
