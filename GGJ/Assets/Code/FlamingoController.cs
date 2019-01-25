12ef0c024e9e579d7eff28d3df1cbe114779b91e
Ilya Voitsekh <ilyvoi@kentor.se>
2019-01-25 22:29:48 +0100

    Created simple AI for enemy
    EnemyController and example of it in FlamingoMovementExample scene

---
+++ GGJ/Assets/Code/FlamingoController.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+
+public enum StateOfEnemy
+{
+    Patrol,
+    Chase,
+    Idle
+}
+
+public class EnemyController : MonoBehaviour
+{
+
+    [SerializeField] private Transform[] pointsToPatrol;
+
+    [SerializeField] private float speed = 1f;
+
+    private StateOfEnemy state = StateOfEnemy.Patrol;
+
+    private int indexOfPatroling = 0;
+
+    private Transform currentTarget;
+
+    void Start ()
+    {
+        currentTarget = pointsToPatrol[indexOfPatroling];
+    }
+	
+	void Update ()
+    {
+        switch (state)
+        {
+            case StateOfEnemy.Patrol:
+                RotateTowardTarget ();
+                MoveTowardsCurrentTarget ();
+                CheckReachingPatrolPoint ();
+                break;
+            case StateOfEnemy.Chase:
+                RotateTowardTarget ();
+                transform.LookAt (currentTarget);
+                MoveTowardsCurrentTarget ();
+                break;
+            case StateOfEnemy.Idle:
+                break;
+            default:
+                break;
+        }
+	}
+
+    void OnTriggerEnter (Collider other) {
+        if (state != StateOfEnemy.Chase && other.tag == "Player")
+        {
+            Chase(other.transform);
+        }
+    }
+
+    void OnTriggerExit (Collider other) {
+        if (other.transform == currentTarget)
+        {
+            Patrol();
+        }
+    }
+
+    void Chase(Transform target)
+    {
+        state = StateOfEnemy.Chase;
+        currentTarget = target;
+        transform.LookAt (currentTarget);
+    }
+
+    void Patrol()
+    {
+        currentTarget = pointsToPatrol[indexOfPatroling];
+        transform.LookAt (currentTarget);
+        state = StateOfEnemy.Patrol;
+        indexOfPatroling = 0;
+    }
+
+    void MoveTowardsCurrentTarget()
+    {
+        transform.position = Vector3.MoveTowards (transform.position, currentTarget.position, Time.deltaTime * speed);
+    }
+
+    void CheckReachingPatrolPoint()
+    {
+        if (transform.position == currentTarget.position) {
+            indexOfPatroling++;
+
+            if (indexOfPatroling == pointsToPatrol.Length) {
+                indexOfPatroling = 0;
+            }
+
+            currentTarget = pointsToPatrol[indexOfPatroling];
+        }
+    }
+
+    void RotateTowardTarget()
+    {
+        var targetRotation = Quaternion.LookRotation(currentTarget.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed* Time.deltaTime);
+    }
+
+}
