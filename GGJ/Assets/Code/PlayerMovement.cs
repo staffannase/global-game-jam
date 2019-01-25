861a4079501b7c2a0da06ce18cb64e72e8d4770a
Adrian <adrian.parneus@gmail.com>
2019-01-25 19:02:39 +0100

    Player Movement Started
    Test scene for player movement.
    basic movement

---
+++ GGJ/Assets/Code/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class PlayerMovement : MonoBehaviour {
+
+    //Public Variables
+    public float movementSpeed = 5f;
+
+    //Private Variables
+    Rigidbody rb;
+
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Get Input
+        float horizontal = Input.GetAxis( "Horizontal" );
+
+        //Calculate movement vector
+        Vector3 movement = new Vector3( horizontal, 0, 0 );
+
+        //Add Speed
+        movement *= movementSpeed;
+
+        //Move!
+        rb.AddForce( movement );
+	}
+}
