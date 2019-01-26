bb289bcc9a8e151542c1a423034fd8d30449e372
joiny7 <joiny4u@hotmail.com>
2019-01-26 16:08:52 +0100

    treeeeeeee

--- GGJ/Assets/Code/WorldTreeScript.cs
+++ GGJ/Assets/Code/WorldTreeScript.cs
@@ -13,29 +13,67 @@ public class WorldTreeScript : MonoBehaviour {
 
     public TreeState state;
     public Transform[] LeafPlaces;
+    public List<GameObject> Leaves;
     public GameObject Trunk;
     public GameObject LeafPrefab;
     public Color SickColourTrunk;
     public Color RevivedColourTrunk;
+    private bool GrowingLeaf;
+    public float growthSpeed;
+
+    public bool test;
 
     void Start () {
         state = TreeState.Lifeless;
-	}
-	
-	void Update () {
-		
+        Leaves = new List<GameObject>();
 	}
 
-    public void AddLife()
+    private void Update()
+    {
+        if (test)
+            StartCoroutine(AddLifeToTree());
+    }
+
+    public IEnumerator AddLifeToTree()
     {
         if (state == TreeState.Lifeless)
         {
-
-            foreach (Transform t in LeafPlaces)
+            foreach(Transform trans in LeafPlaces)
             {
-                var leaf = Instantiate(LeafPrefab);
+                var leaf = Instantiate(LeafPrefab, trans.transform.position, Quaternion.identity);
+                Leaves.Add(leaf);
+                GrowingLeaf = true;
+
+                while (GrowingLeaf)
+                {
+                    leaf.transform.localScale *= Time.deltaTime * growthSpeed;
+
+                    if (leaf.transform.localScale.x > 3f)
+                        GrowingLeaf = false;
+                }
             }
 
+            state = TreeState.Sick;
+            yield break;
+        }
+        else if (state == TreeState.Sick)
+        {
+            foreach (GameObject leaf in Leaves)
+            {
+                GrowingLeaf = true;
+
+                while (GrowingLeaf)
+                {
+                    leaf.transform.localScale *= Time.deltaTime * growthSpeed;
+                    // make it greeeeeeeeeeen
+
+                    if (leaf.transform.localScale.x > 5f)
+                        GrowingLeaf = false;
+                }
+            }
+
+            state = TreeState.Revived;
+            yield break;
         }
     }
 }
