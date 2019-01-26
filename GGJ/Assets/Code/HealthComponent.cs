b2b2d1e727ebc0472471e879b76116769da51e47
Ilya Voitsekh <ilyvoi@kentor.se>
2019-01-26 13:19:03 +0100

    Implemented health, enemy attack and test game over

---
+++ GGJ/Assets/Code/HealthComponent.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class HealthComponent : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent deadEvent;
+
+    [SerializeField]
+    private UnityEvent damageEvent;
+
+    [SerializeField]
+    private int hp;
+
+    public bool IsDead { get; set; }
+
+    public void GetDamage()
+    {
+        if (!IsDead)
+        {
+            hp--;
+            damageEvent.Invoke();
+            CheckForDead ();
+        }
+    }
+
+    void CheckForDead()
+    {
+        IsDead = hp == 0;
+        if(IsDead)
+            deadEvent.Invoke();
+    }
+}
---
+++ GGJ/Assets/Code/HealthIndicator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class HealthIndicator : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void DecreaseHealth()
+    {
+        spriteRenderer.color = new Color(spriteRenderer.color.r/2f, spriteRenderer.color.g/2f, spriteRenderer.color.b/2f);
+    }
+}
